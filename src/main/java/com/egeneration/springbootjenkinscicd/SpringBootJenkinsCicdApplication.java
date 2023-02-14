bcdda09519bc8e0347ddf4dd97e3591db4a4c8b3
muhammad mahadi <muhammad.mahadi@egeneration.co>
2023-02-14 14:41:11 +0600

    initial commit

---
+++ src/main/java/com/egeneration/springbootjenkinscicd/SpringBootJenkinsCicdApplication.java
@@ -0,0 +1,13 @@
+package com.egeneration.springbootjenkinscicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJenkinsCicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJenkinsCicdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/egeneration/springbootjenkinscicd/SpringBootJenkinsCicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.egeneration.springbootjenkinscicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJenkinsCicdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
